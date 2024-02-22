87dfd525287d971ea5663afd370190b11b5977ce
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-02-23 02:22:45 +0530

    created a file for private & open route path

--- src/components/core/Auth/PrivateRoute.jsx
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+const PrivateRoute = ({children}) => {
+  const {token} = useSelector((state) => state.auth)
+
+  if(token === null) {
+    return children
+  } else {
+    return <Navigate to="/login" />
+  }
 }
 
-export default PrivateRoute
+export default PrivateRoute;
--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import { toast } from "react-hot-toast";
 
-const authAPI = () => {
-  return (
-    <div>authAPI</div>
-  )
+import { setLoading, setToken } from "../../slices/authSlice";
+
+const {
+  SENDOTP_API,
+  SIGNUP_API,
+  LOGIN_API,
+  RESETPASSTOKEN_API,
+  RESETPASSWORD_API
+} = endpoints
+
+export function sendOtp(email, navigate) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    dispatch(setLoading(true))
+    try {
+      const response = await apiConnector("POST", SENDOTP_API, {
+        email,
+        checkUserPresent: true
+      })
+      console.log("SENDOTP API RESPONSE..........", response)
+
+      console.log(response.data.message)
+
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+
+      toast.success("OTP Sent Successfully")
+      navigate("/verify-email")
+    } catch (error) {
+      console.log("SENDOTP API ERROR......", error)
+      toast.error("Could Not Send OTP")
+    }
+    dispatch(setLoading(false))
+    toast.dismiss(toastId)
+  }
 }
 
-export default authAPI
+export function signUp (
+  accountType,
+  firstName,
+  lastName,
+  email,
+  password,
+  confirmPassword,
+  otp,
+  navigate
+) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    dispatch(setLoading)
+    try {
+      const response = await apiConnector("POST", SIGNUP_API, {
+        accountType,
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+        otp,
+      })
+
+      console.log("SIGNUP_API RESPONSE........", response)
+
+      if (!response.data.success) {
+        throw new Error(response.data.message)
+      }
+      toast.success("Signup Successful")
+      navigate("/login")
+    } catch (error) {
+      console.log("SIGNUP API ERROR.......", error)
+      toast.error("Signup Failed")
+      navigate("/signup")
+    }
+    dispatch(setLoading(false))
+    toast.dismiss(toastId)
+  }
+}
